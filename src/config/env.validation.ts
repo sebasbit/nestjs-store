fcd302cc9d4adbae7afad4b6bce1cae2bf830c90
sebasbit <sebastian.torres0426@gmail.com>
2025-02-22 17:43:31 -0500

    build: add services in docker-compose
    Signed-off-by: sebasbit <sebastian.torres0426@gmail.com>

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -4,7 +4,7 @@ export default Joi.object({
   NODE_ENV: Joi.string()
     .valid('development', 'production', 'test')
     .default('development'),
-  DB_HOST: Joi.string().ip().required(),
+  DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().port().required(),
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
