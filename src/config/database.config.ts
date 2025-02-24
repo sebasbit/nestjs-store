0a8a94fe0419091906a5c545565234658d79f634
sebasbit <sebastian.torres0426@gmail.com>
2025-02-24 16:24:10 -0500

    feat: add configuration for migrations
    Signed-off-by: sebasbit <sebastian.torres0426@gmail.com>

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,7 +5,7 @@ export default registerAs('database', () => ({
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   type: 'postgres' as any,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
